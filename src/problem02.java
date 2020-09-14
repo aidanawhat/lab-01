21827144fa06b6e74713a22e7fe79680b966b5c6
Aidana Kyzylzharova <kyzylzharova_a@auca.kg>
2020-09-15 00:09:00 +0600

    Initial commit

---
+++ src/problem02.java
@@ -0,0 +1,11 @@
+public class problem02 {
+    public static void main(String[] args) {
+        System.out.println("***************");
+        //
+        System.out.println("*Hello, World*");
+        //
+        System.out.println("***************");
+        //
+
+
+    }}
