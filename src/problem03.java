4e9f65edf08e5b3216c517cdad3711ff3b86a05d
Aidana Kyzylzharova <kyzylzharova_a@auca.kg>
2020-09-20 23:17:24 +0600

    Finish Problem 3 in Lab 1

---
+++ src/problem03.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class problem03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+       String firstName;
+       System.out.print("What is your name? ");
+       firstName = scanner.next();
+       System.out.print("Hello, " + firstName + "!");
+
+
+    } }
